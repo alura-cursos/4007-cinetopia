0324e8171f67b5b8c30444ead9ac3161c993fa21
ikyrie <mah.c5r@gmail.com>
2024-11-07 12:11:21 -0300

    video4.3

--- lib/app/services/search_movies_service.dart
+++ lib/app/services/search_movies_service.dart
@@ -5,15 +5,29 @@ import 'package:http/http.dart' as http;
 class SearchMoviesService {
   final String popularMoviesUrl = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
 
+  List<Movie> movies = <Movie>[];
+
   Future<List<Movie>> searchMovies() async {
     try {
-      await http.get(
+      final response = await http.get(
         Uri.parse(popularMoviesUrl),
         headers: {
           'accept': 'application/json',
           'Authorization': 'Bearer $apiKey'
         },
       );
-    } catch (e) {}
+      if (response.statusCode == 200) {
+        print(response.body);
+      }
+
+      else {
+        throw Exception(response.body);
+      }
+      return movies;
+
+    } catch (e) {
+      print(e);
+      return movies;
+    }
   }
 }
