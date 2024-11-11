04d193dbc23f982b190ca80c4b25ceb6702d0880
ikyrie <mah.c5r@gmail.com>
2024-11-11 15:14:24 -0300

    Mão na massa 3

--- lib/app/helpers/consts.dart
+++ lib/app/helpers/consts.dart
@@ -3,6 +3,8 @@ import 'package:cinetopia/app/api_key.dart';
 const String popularMoviesUrl =
     "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
 
+const String upcomingUrl = "https://api.themoviedb.org/3/movie/upcoming?language=en-US&region=US&page=1";
+
 const String moviePrefixUrl = "https://api.themoviedb.org/3/search/movie?query=";
 
 const String movieFilterSulfix = "&include_adult=false&language=en-US&page=1';";
