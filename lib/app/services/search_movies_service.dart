1bfa963facbcec089b07d651cd5f5f08d621af91
ikyrie <mah.c5r@gmail.com>
2024-11-11 14:39:13 -0300

    video5.1

--- lib/app/services/search_movies_service.dart
+++ lib/app/services/search_movies_service.dart
@@ -1,22 +1,25 @@
 import 'dart:convert';
 
-import 'package:cinetopia/app/api_key.dart';
+import 'package:cinetopia/app/helpers/consts.dart';
 import 'package:cinetopia/app/models/movie.dart';
 import 'package:http/http.dart' as http;
 
-class SearchMoviesService {
+abstract class SearchMoviesService {
+  Future<List<Movie>> getMovies();
+}
+
+class SearchPopularMoviesService implements SearchMoviesService {
+
   final String popularMoviesUrl = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
 
   List<Movie> movies = <Movie>[];
 
-  Future<List<Movie>> searchMovies() async {
+  @override
+  Future<List<Movie>> getMovies() async {
     try {
       final response = await http.get(
         Uri.parse(popularMoviesUrl),
-        headers: {
-          'accept': 'application/json',
-          'Authorization': 'Bearer $apiKey'
-        },
+        headers: requestHeader,
       );
       if (response.statusCode == 200) {
         for (dynamic movie in json.decode(response.body)['results']) {
